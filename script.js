506347bbe280c99d75561604b611969f40b29f65
Pranav Jadhav <pranavjadhav0298@gmail.com>
2026-09-16 21:44:24 +0530

    Updated js file with backend link

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "http://127.0.0.1:8000/predict";
+const API_URL = "https://mental-health-score-fuep.onrender.com";
 const PLATFORMS = [
   { value: "Facebook", icon: "📘" },
   { value: "LinkedIn", icon: "💼" },
